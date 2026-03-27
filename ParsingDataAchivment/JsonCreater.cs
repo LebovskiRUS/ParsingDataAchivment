1173c41f8ee17064ee5a4bd36a56acd37694612a
Gouler <Gouler@DESKTOP-QOSTM1V>
2026-03-27 21:13:17 +0300

    добавление патентов и список авторов

--- ParsingDataAchivment/JsonCreater.cs
+++ ParsingDataAchivment/JsonCreater.cs
@@ -8,6 +8,8 @@ namespace ParsingDataAchivment
 {
     internal class JsonCreater : ParsingTitleList
     {
+        private readonly string _jsonFilePath = "patent.json";
+        private readonly string _jsonFilePathId = "id_patent.json";
         public JsonCreater(IWebElement xpath, IWebDriver driver, string id) : base(xpath, driver, id)
         {
            
@@ -15,6 +17,7 @@ namespace ParsingDataAchivment
 
         public void CreateJson()
         {
+
             var data = new
             {
                 id = Convert.ToInt32(Application),
@@ -26,15 +29,41 @@ namespace ParsingDataAchivment
                 data_public = DataPublic,
                 citatioin = ListDocumentCitationInReport,
                 communicate = AdresToCommunication,
-                author = Author,
+                author = SplitAuthor(Author),
                 patent_holder = PatentHolder,
                 title = Title,
                 color = Color
 
             };
-            string json = JsonConvert.SerializeObject(data, Formatting.Indented);
-            File.WriteAllText("patent.json", json);
-            Console.WriteLine("json создался");
+
+            // Читаем существующие патенты или создаем новый список
+            List<object> patents;
+            if (File.Exists(_jsonFilePath))
+            {
+                string existingJson = File.ReadAllText(_jsonFilePath);
+                patents = JsonConvert.DeserializeObject<List<object>>(existingJson) ?? new List<object>();
+            }
+            else
+            {
+                patents = new List<object>();
+            }
+
+            // Добавляем новый патент
+            patents.Add(data);
+
+            // Сохраняем обновленный список
+            string updatedJson = JsonConvert.SerializeObject(patents, Formatting.Indented);
+            File.WriteAllText(_jsonFilePath, updatedJson);
+
+            Console.WriteLine($"Патент {Application} добавлен в JSON. Всего патентов: {patents.Count}");
+        }
+
+        private List<string> SplitAuthor(string authorString)
+        {
+            if(string.IsNullOrEmpty(authorString)) 
+                return new List<string>();
+            var authors = authorString.Split([",\r\n",","], StringSplitOptions.RemoveEmptyEntries);
+            return authors.Select(x => x.Trim()).ToList();
         }
     }
 }
